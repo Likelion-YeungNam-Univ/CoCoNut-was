08c63805ad82d80a37d358608df9030e5ebae005
eastminnn <oh1334000@gmail.com>
2025-08-05 18:36:40 +0900

    chore : 환경세팅

---
+++ src/test/java/CoCoNut_was/CoCoNutWasApplicationTests.java
@@ -0,0 +1,13 @@
+package CoCoNut_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCoNutWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
