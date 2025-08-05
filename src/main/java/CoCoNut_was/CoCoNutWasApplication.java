09f7811e98fb290a4af80a6543d13a1fe46ede80
eastminnn <oh1334000@gmail.com>
2025-08-05 18:36:40 +0900

    feat : 개발 환경세팅

---
+++ src/main/java/CoCoNut_was/CoCoNutWasApplication.java
@@ -0,0 +1,13 @@
+package CoCoNut_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCoNutWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCoNutWasApplication.class, args);
+	}
+
+}
