2e561bbb87cce93b36b0f35aa316a4f1a4f9b44c
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-09 11:25:43 +0900

    feat : 제출물 엔티티 정의
    제출물에 대한 기본적인 엔티티만 정의하였습니다.

---
+++ src/main/java/CoCoNut_was/domains/submission/entity/Submission.java
@@ -0,0 +1,44 @@
+package CoCoNut_was.domains.submission.entity;
+
+import CoCoNut_was.domains.project.entitiy.Project;
+import CoCoNut_was.domains.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Submission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private LocalDate submittedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder public Submission(String description, String imageUrl, LocalDate submittedAt, Project project, User user) {
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.submittedAt = submittedAt;
+        this.project = project;
+        this.user = user;
+    }
+}
