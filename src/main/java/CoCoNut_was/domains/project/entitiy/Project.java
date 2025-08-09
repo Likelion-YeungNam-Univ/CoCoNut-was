7150bc632f316bab155cb6be4c52dbfbd8b45d8b
eastminnn <oh1334000@gmail.com>
2025-08-09 14:14:28 +0900

    feat : 공모전 엔티티 빌더 수정

--- src/main/java/CoCoNut_was/domains/project/entitiy/Project.java
+++ src/main/java/CoCoNut_was/domains/project/entitiy/Project.java
@@ -1,5 +1,6 @@
 package CoCoNut_was.domains.project.entitiy;
 
+import CoCoNut_was.domains.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -15,6 +18,10 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 공모전 고유 키 값
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 사용자와 N:1
 
     @Column(nullable = false, length = 100)
     private String title; // 공모전 제목
@@ -49,18 +56,30 @@ public class Project {
     @Column(nullable = false)
     private Status status; // 공모전 상태
 
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectColor> projectColors = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectStyle> projectStyles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectTarget> projectTargets = new ArrayList<>();
+
     @PrePersist
     public void onCreate() { // 생성일자 자동 생성
         this.createdAt = LocalDateTime.now();
+        this.status = Status.IN_PROGRESS; // 진행중 상태로 시작
     }
 
     @Builder
-    public Project(String title, String merchantName, Category category, String description,
-                   String period, int rewardAmount, String summary, LocalDateTime createdAt,
-                   Status status) {
+    public Project(User user, String title, String merchantName, Category category,
+                   BusinessType businessType, String description, String period, int rewardAmount,
+                   String summary, LocalDateTime createdAt, Status status) {
+        this.user = user;
         this.title = title;
         this.merchantName = merchantName;
         this.category = category;
+        this.businessType = businessType;
         this.description = description;
         this.period = period;
         this.rewardAmount = rewardAmount;
