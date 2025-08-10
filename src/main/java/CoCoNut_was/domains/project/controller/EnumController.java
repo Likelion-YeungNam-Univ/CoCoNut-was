7640710be56283f01b7ac9d90a438cf99353b40f
eastminnn <oh1334000@gmail.com>
2025-08-10 19:09:41 +0900

    feat : 카테고리, 업종 dto, controller 추가

---
+++ src/main/java/CoCoNut_was/domains/project/controller/EnumController.java
@@ -0,0 +1,32 @@
+package CoCoNut_was.domains.project.controller;
+
+import CoCoNut_was.domains.project.dto.EnumResponseDto;
+import CoCoNut_was.domains.project.entity.BusinessType;
+import CoCoNut_was.domains.project.entity.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/enums")
+public class EnumController {
+    @GetMapping("/categories")
+    public ResponseEntity<?> getCategories() { // 카테고리 조회
+        List<EnumResponseDto> categories = Arrays.stream(Category.values())
+                .map(category -> new EnumResponseDto(category.name(), category.getDescription()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(categories);
+    }
+    @GetMapping("/businessTypes")
+    public ResponseEntity<?> getBusinessType() { // 업종 조회
+        List<EnumResponseDto> businessTypes = Arrays.stream(BusinessType.values())
+                .map(businessType -> new EnumResponseDto(businessType.name(), businessType.getDescription()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(businessTypes);
+    }
+}
