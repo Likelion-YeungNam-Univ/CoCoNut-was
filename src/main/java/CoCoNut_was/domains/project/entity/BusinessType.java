a2f3144d414dbda41c1baa935803d6f9b4f0aac9
eastminnn <oh1334000@gmail.com>
2025-08-10 19:36:05 +0900

    refactor : 업종 설명 변경

--- src/main/java/CoCoNut_was/domains/project/entity/BusinessType.java
+++ src/main/java/CoCoNut_was/domains/project/entity/BusinessType.java
@@ -6,14 +6,14 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum BusinessType {
-    FOOD_BEVERAGE("식당 / 카페 / 주점"),
-    RETAIL_COMMERCE("의류 / 쇼핑물"),
-    BEAUTY_HEALTH("뷰티 / 헬스"),
-    EDUCATION("교육 / 학원"),
-    MEDICAL("병원 / 약국"),
-    CULTURE_LEISURE("문화 / 여가"),
-    PROFESSIONAL_SERVICE("서비스 / 전문직"),
-    ACCOMMODATION("숙박 / 관광"),
+    FOOD_BEVERAGE("식당/카페/주점"),
+    RETAIL_COMMERCE("의류/쇼핑물"),
+    BEAUTY_HEALTH("뷰티/헬스"),
+    EDUCATION("교육/학원"),
+    MEDICAL("병원/약국"),
+    CULTURE_LEISURE("문화/여가"),
+    PROFESSIONAL_SERVICE("서비스/전문직"),
+    ACCOMMODATION("숙박/관광"),
     ETC("기타");
 
     private final String description;
