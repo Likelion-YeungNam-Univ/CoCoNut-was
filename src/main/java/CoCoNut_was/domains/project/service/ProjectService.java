ff90677bf7ee78e64845c76c686bf0ff457d6bf7
eastminnn <oh1334000@gmail.com>
2025-08-11 15:30:32 +0900

    feat : 성능을 위한 읽기 전용 트랜잭션 추가

--- src/main/java/CoCoNut_was/domains/project/service/ProjectService.java
+++ src/main/java/CoCoNut_was/domains/project/service/ProjectService.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProjectService {
     private final ProjectRepository projectRepository;
     private final ProjectColorRepository projectColorRepository;
