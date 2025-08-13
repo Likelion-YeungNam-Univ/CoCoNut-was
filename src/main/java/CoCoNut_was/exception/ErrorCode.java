19beb87053e20c3272de426dd0c21c7f243140d7
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-13 23:50:21 +0900

    feat : 빈 프롬프트 커스텀 예외처리

--- src/main/java/CoCoNut_was/exception/ErrorCode.java
+++ src/main/java/CoCoNut_was/exception/ErrorCode.java
@@ -24,7 +24,11 @@ public enum ErrorCode {
     SUBMISSION_USER_NOT_MATCHED(403, "작품의 유저정보와 로그인 정보가 일치하지 않습니다."),
     REQUIRED_SUBMISSION_INFO(400, "제출물 형태에 대해 누락이 있습니다. 반드시 KEY에 info를 포함하고, " +
             "VALUE에 JSON값을, Content-Type를 application/json으로 설정해주세요."),
-    IMAGE_UPLOAD_FAILED(500, "제출한 이미지 업로드에 예기치 않은 문제가 발생하였습니다.");
+    IMAGE_UPLOAD_FAILED(500, "제출한 이미지 업로드에 예기치 않은 문제가 발생하였습니다."),
+
+
+    // AI 관련
+    PROMPT_IS_BLANK(400, "프롬프트 내용이 없습니다. 제대로 입력해주세요.");
 
 
 
