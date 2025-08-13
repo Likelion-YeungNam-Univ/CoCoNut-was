40fe59b7da547c980ae1fd4069b6ffd569bbb4f0
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-14 00:08:51 +0900

    feat : 프롬프트에 대한 DTO 생성(Project,Submission)

---
+++ src/main/java/CoCoNut_was/domains/submission/reqdto/SubmissionPromptDto.java
@@ -0,0 +1,13 @@
+package CoCoNut_was.domains.submission.reqdto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class SubmissionPromptDto {
+
+    @Schema(description = "AI에게 전달할 프롬프트 내용", example = "우리는 카페 아기사자의 메뉴판을 만들어주는 공모전에 지원했어. 아기사자의 밝은 갈색과 초원 색상을 잘 섞었어. 이러한 설명을 좀 자세히 적어줘")
+    @NotBlank(message = "프롬프트 내용은 비워둘 수 없습니다.")
+    private String prompt;
+}
