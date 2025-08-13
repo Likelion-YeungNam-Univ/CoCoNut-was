4859f04a830cfdf9f234edab26d5fb1f3506faaa
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-14 00:14:12 +0900

    fix : Project 프롬프트 예시 오기입 수정

--- src/main/java/CoCoNut_was/domains/project/dto/ProjectPromptDto.java
+++ src/main/java/CoCoNut_was/domains/project/dto/ProjectPromptDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class ProjectPromptDto {
-    @Schema(description = "AI에게 전달할 프롬프트 내용", example = "우리는 카페 아기사자의 메뉴판을 만들어주는 공모전에 지원했어. 아기사자의 밝은 갈색과 초원 색상을 잘 섞었어. 이러한 설명을 좀 자세히 적어줘")
+    @Schema(description = "AI에게 전달할 프롬프트 내용", example = "대학생들을 타겟으로 하는 카페 로고 디자인 공모전을 열고 싶어")
     @NotBlank(message = "프롬프트 내용은 비워둘 수 없습니다.")
     private String prompt;
 }
