072465b4fb8c53dc93b43ca8aa4a24c248083f9f
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-14 18:57:26 +0900

    feat : 제출 DTO 연관링크 필드 추가

--- src/main/java/CoCoNut_was/domains/submission/reqdto/SubmitDto.java
+++ src/main/java/CoCoNut_was/domains/submission/reqdto/SubmitDto.java
@@ -19,6 +19,9 @@ public class SubmitDto {
     @Schema(description = "공모전 작품 설명", example = "아기사자의 그림이 그려져있고, 아이들이 좋아할만한 캐릭터 디자인을 채택하였습니다.")
     private String description;
 
+    @Schema(description = "작품에 대한 외부링크", example = "https://깃허브주소.com")
+    private String relatedUrl;
+
     // 제출 일자는 그냥 LocalDate.now()를 쓰면 되지 않을까 라는 생각입니다.
 
 //    @NotBlank(message = "제출일자는 필수 입력입니다.")
@@ -29,6 +32,7 @@ public class SubmitDto {
         return Submission.builder()
                 .title(this.title)
                 .description(this.description)
+                .relatedUrl(this.relatedUrl)
                 .imageUrl(imageUrl)
                 .submittedAt(LocalDate.now())
                 .project(project)
