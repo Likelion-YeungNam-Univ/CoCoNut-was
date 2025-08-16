ea7d35301d0097df1144e95582ce65b0b4288c28
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-16 16:20:43 +0900

    feat : 투표 자격요건 로직 추가

--- src/main/java/CoCoNut_was/domains/vote/service/VoteService.java
+++ src/main/java/CoCoNut_was/domains/vote/service/VoteService.java
@@ -46,17 +46,20 @@ public class VoteService {
 
         // 4. 투표 가능한 사람인지
         // 4-1. 해당 프로젝트에 투표한 이력이 있는 사람인지
-
+        if (voteRepository.existsByUserAndSubmission_Project(user, project))
+            throw new CustomException(ErrorCode.INVALID_DUPLICATE_VOTE);
 
 
         // 4-2. 공모전을 만든 소상공인은 투표할 수 없음
-
+        if (user.equals(project.getUser()))
+            throw new CustomException(ErrorCode.INVALID_OWN_PROJECT_VOTE);
 
 
         // 4-3. 참여자는 자기 작품에 투표할 수 없음
+        if (user.equals(submission.getUser()))
+            throw new CustomException(ErrorCode.INVALID_SELF_VOTE);
 
-
-
+        
         // 5. 투표 처리
     }
 }
