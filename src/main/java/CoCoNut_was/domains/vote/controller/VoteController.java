97e67955a6cc8ae61609a0b4d671869f2931b2f9
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-16 14:58:11 +0900

    feat : 기본 클래스들 관련 세팅

--- src/main/java/CoCoNut_was/domains/vote/controller/VoteController.java
+++ src/main/java/CoCoNut_was/domains/vote/controller/VoteController.java
@@ -1,5 +1,6 @@
 package CoCoNut_was.domains.vote.controller;
 
+import CoCoNut_was.domains.vote.service.VoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/projects/{project_id}/votes")
 public class VoteController {
-    
+    private final VoteService voteService;
 
+
+    // 1. 투표 등록
+
+
+    // 2. 투표 결과 전체 조회
+
+
+    // 3. 한 작품에 대한 투표 개수
+
+
+    // 4. 투표 삭제 (이거 필요할까요)
 
 
 }
--- src/main/java/CoCoNut_was/domains/vote/service/VoteService.java
+++ src/main/java/CoCoNut_was/domains/vote/service/VoteService.java
@@ -1,4 +1,11 @@
 package CoCoNut_was.domains.vote.service;
 
+import CoCoNut_was.domains.vote.repository.VoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class VoteService {
+    private final VoteRepository voteRepository;
 }
