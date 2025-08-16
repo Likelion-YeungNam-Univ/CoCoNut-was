6815a8550105c47fd9804c96245d0a73733fd30f
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-16 17:35:04 +0900

    feat : 투표 개수 조회 DTO 구현

---
+++ src/main/java/CoCoNut_was/domains/vote/resdto/VoteCountDto.java
@@ -0,0 +1,19 @@
+package CoCoNut_was.domains.vote.resdto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class VoteCountDto {
+    private Long projectId;
+    private Long submissionId;
+    private Long voteCount;
+
+    @Builder public VoteCountDto(Long projectId, Long submissionId, Long voteCount) {
+        this.projectId = projectId;
+        this.submissionId = submissionId;
+        this.voteCount = voteCount;
+    }
+}
