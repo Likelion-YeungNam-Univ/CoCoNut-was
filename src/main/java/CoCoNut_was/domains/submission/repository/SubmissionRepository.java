7799d83b53e456d7376db6041c990e754e04a826
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-19 10:27:29 +0900

    fix : 작품이 단 한번만 제출 가능했던 문제 해결

--- src/main/java/CoCoNut_was/domains/submission/repository/SubmissionRepository.java
+++ src/main/java/CoCoNut_was/domains/submission/repository/SubmissionRepository.java
@@ -1,5 +1,6 @@
 package CoCoNut_was.domains.submission.repository;
 
+import CoCoNut_was.domains.project.entity.Project;
 import CoCoNut_was.domains.submission.entity.Submission;
 import CoCoNut_was.domains.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +13,7 @@ import java.util.Optional;
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
     Optional<Submission> findByUser(User user);
 
-    boolean existsByUser(User user);
-
     List<Submission> findByProjectId(Long project_id);
+
+    boolean existsByUserAndProject(User user, Project project);
 }
