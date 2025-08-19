f5b95359d8e935bb2ca210aff061e9023ae67b0b
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-19 19:47:12 +0900

    feat : 제출기한 마감 Swagger 작성

--- src/main/java/CoCoNut_was/domains/submission/controller/SubmissionApi.java
+++ src/main/java/CoCoNut_was/domains/submission/controller/SubmissionApi.java
@@ -45,6 +45,12 @@ public interface SubmissionApi {
                                         "status": 400,
                                         "message": "이미 지원하신 공모전에 다시 지원할 수 없습니다."
                                     }
+                                    """),
+                            @ExampleObject(name = "제출 기한 마감", value = """
+                                    {
+                                        "status": 400,
+                                        "message": "제출기한이 이미 지났습니다."
+                                    }
                                     """)
                     })),
             @ApiResponse(responseCode = "401", description = "액세스 토큰 미입력/만료",
