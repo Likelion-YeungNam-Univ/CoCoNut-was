25e115cf9b38962ba0a3859bfa454344443c5e55
eastminnn <oh1334000@gmail.com>
2025-08-21 20:28:42 +0900

    feat : 공모전 스케줄링을 위한 쿼리 추가

--- src/main/java/CoCoNut_was/domains/project/Repository/ProjectRepository.java
+++ src/main/java/CoCoNut_was/domains/project/Repository/ProjectRepository.java
@@ -1,13 +1,19 @@
 package CoCoNut_was.domains.project.Repository;
 
 import CoCoNut_was.domains.project.entity.Project;
+import CoCoNut_was.domains.project.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     // 공모전 조회할 때 User 엔티티 함께 조회 (N+1 방지)
     @Query("SELECT p FROM Project p JOIN FETCH p.user u ORDER BY p.createdAt DESC")
     List<Project> findAllWithUser();
+
+    List<Project> findAllByDeadlineBeforeAndStatus(LocalDate date, Status status);
+
+    List<Project> findAllByStatusAndVotingStartDateBefore(Status status, LocalDate date);
 }
