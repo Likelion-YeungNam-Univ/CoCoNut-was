69e8fdca3d055d3b29a5c26cc7884e998763371f
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-23 15:32:23 +0900

    feat : 투표조회, 로그인 사용자 투표여부 필드 추가

--- src/main/java/CoCoNut_was/domains/vote/resdto/VoteCountDto.java
+++ src/main/java/CoCoNut_was/domains/vote/resdto/VoteCountDto.java
@@ -10,10 +10,12 @@ public class VoteCountDto {
     private Long projectId;
     private Long submissionId;
     private Long voteCount;
+    private boolean isVoted;
 
-    @Builder public VoteCountDto(Long projectId, Long submissionId, Long voteCount) {
+    @Builder public VoteCountDto(Long projectId, Long submissionId, Long voteCount, boolean isVoted) {
         this.projectId = projectId;
         this.submissionId = submissionId;
         this.voteCount = voteCount;
+        this.isVoted = isVoted;
     }
 }
