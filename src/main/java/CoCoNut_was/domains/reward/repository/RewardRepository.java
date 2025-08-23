f955e4b3be811aef75a032339875cdb707523f7f
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-23 15:06:59 +0900

    feat : 프로젝트 기반 수상 엔티티 검색 메소드 추가

--- src/main/java/CoCoNut_was/domains/reward/repository/RewardRepository.java
+++ src/main/java/CoCoNut_was/domains/reward/repository/RewardRepository.java
@@ -6,6 +6,7 @@ import CoCoNut_was.domains.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RewardRepository extends JpaRepository<Reward, Long> {
     // 특정 사용자의 모든 보상 내역을 최신순으로 조회
@@ -18,4 +19,6 @@ public interface RewardRepository extends JpaRepository<Reward, Long> {
     long countByUser(User user);
 
     boolean existsByUserAndProject(User user, Project project);
+
+    Optional<Reward> findByProject(Project project);
 }
