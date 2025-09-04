08986a5fbdb1471c2128d9195152bbae525863ec
eastminnn <oh1334000@gmail.com>
2025-09-04 17:24:58 +0900

    fix : 스케줄링을 위한 어노테이션 추가

--- src/main/java/CoCoNut_was/CoCoNutWasApplication.java
+++ src/main/java/CoCoNut_was/CoCoNutWasApplication.java
@@ -2,8 +2,10 @@ package CoCoNut_was;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoCoNutWasApplication {
 
 	public static void main(String[] args) {
